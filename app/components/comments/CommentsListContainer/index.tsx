bbbcf50c6399415724c32037133ef3812f00308a
Ester Monteiro <esterpmonteiro@gmail.com>
2024-05-27 10:47:11 +0200

    Scrolling to recent created comment

--- app/components/comments/CommentsListContainer/index.tsx
+++ app/components/comments/CommentsListContainer/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { FlatList, Text, View } from 'react-native';
 import translate from '../../../i18n';
 import { useAppSelector } from '../../../store';
@@ -7,16 +8,21 @@ import CommentsList from '../CommentsList';
 const CommentListContainer = () => {
   const comments = useAppSelector((state) => state.comments?.allComments);
   const replies = getSortedReplies(comments);
+  const containerRef = React.useRef<FlatList>(null);
 
   return (
     <View>
       {replies ? (
         <FlatList
+          ref={containerRef}
           data={replies}
           renderItem={({ item }) => (
             <CommentsList commentReplies={[item]} replyLevel={0} />
           )}
           keyExtractor={(item) => item.comment.id}
+          onContentSizeChange={() =>
+            containerRef.current?.scrollToEnd({ animated: true })
+          }
         />
       ) : (
         <Text>{translate('commentsListContainer.noCommentsMessage')}</Text>
